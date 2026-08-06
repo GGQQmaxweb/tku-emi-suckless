f8a702aaa451474108e7e925c7b6e496378cde7a
GGQQmaxweb <145407967+GGQQmaxweb@users.noreply.github.com>
2026-08-06 15:02:37 +0200

    feat: trigger course loading on dashboard init and apply minor code style cleanups

--- gui/script.js
+++ gui/script.js
@@ -30,6 +30,7 @@ async function updateUserLocalData() {
     await getDashboardData();
     await updateProgressBar();
     await userInformation();
+    loadCourses();
     document.getElementById('UpdataUserdataBtn').innerText = "Update User Data";
 }
 
@@ -325,7 +326,7 @@ function renderScheduleMyClass(schedule, days, periods) {
 }
 
 async function searchClassTimeClick(time) {
-    const searchTime = `${time.day},${Number(time.period)}`    
+    const searchTime = `${time.day},${Number(time.period)}`
     document.getElementById('search-time').value = searchTime
     runSearch({
         times: searchTime,
@@ -391,7 +392,7 @@ async function updateScheduleMyClass(options) {
 
     // Always render, even when schedule is empty
     renderScheduleMyClass(schedule, days, periods);
-    document.getElementById("load-my-class-section-btn").innerText="Load My Class"
+    document.getElementById("load-my-class-section-btn").innerText = "Load My Class"
 }
 
 async function exportScheduleClass() {
@@ -554,7 +555,7 @@ async function runSearch(options = {}) {
 
 }
 
-async function searchButtonClick(){
+async function searchButtonClick() {
 
     runSearch({
         title: document.getElementById("search-title").value,
@@ -593,7 +594,7 @@ async function handleLogin() {
     const user = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
     document.getElementById('loginbtn').innerText = "Login check took time please wait"
-    
+
     const success = await window.pywebview.api.authenticate(user, pass);
 
     if (success) {
